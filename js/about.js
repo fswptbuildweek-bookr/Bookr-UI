04d8e97795eb0d02d5e2d5c9ce01486cb5bb21b2
jacquetta <jacquettavb@hotmail.com>
2019-02-20 01:31:25 -0600

    updated JavaScript Menu

--- js/about.js
+++ js/about.js
@@ -1,16 +1,46 @@
-class Dropdown {
-    constructor(element){
-        this.element = element;
-        this.button = element.querySelector('div.menu-button');
-        this.content = element.querySelector('.dropdown-content');
-        
-        this.button.addEventListener('click', () => {
-            this.toggleMenu();
-        })
-    }
-    toggleMenu() {
-        this.content.classList.toggle('dropdown-hidden');
-    }
-}
+document.addEventListener('DOMContentLoaded', function(){
+    const $dropdowns = getAll('.navbar-item.has-dropdown:not(.is-hoverable)');
 
-let dropdowns = document.querySelectorAll('.dropdown').forEach(dropdown => new Dropdown(dropdown));
+    if($dropdowns.length > 0){
+        $dropdowns.forEach(function ($el) {
+            $el.addEventListener('click', event =>{
+                event.stopPropagation();
+                $el.classList.toggle('is-active');
+            });
+        });
+
+        document.addEventListener('click', event => {
+            closeDropdowns();
+        });
+    }
+
+    function closeDropdowns(){
+        $dropdowns.forEach($el => {
+            $el.classList.remove('is-active');
+        });
+    }
+
+    document.addEventListener('keydown', event => {
+        const e = event || window.event;
+        if(e.keycode === 27){
+            closeDropdowns();
+        }
+    });
+
+    const $burgers = getAll('.burger');
+
+    if($burgers.length > 0){
+        $burgers.forEach($el => {
+            $el.addEventListener('click', function () {
+                const target = $el.dataset.target;
+                const $target = document.getElementById(target);
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+
+    function getAll(selectors){
+        return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+    }
+});
