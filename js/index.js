aa1c5ae12d4efa053bb75a4495cce7d24c3902d6
Jacquetta B <jacquettavb@hotmail.com>
2019-02-23 11:39:22 -0600

    removed signin alert

--- js/index.js
+++ js/index.js
@@ -5,10 +5,3 @@ login.addEventListener('click', (event) => {
     alert('Sending You to Bookr Login');
 } );
 
-
-const signUp = document.querySelector('.btns');
-signUp.setAttribute('id', 'signUp');
-
-signUp.addEventListener('click', (event) => {
-    alert('Sign-Up for Bookr');
-});
